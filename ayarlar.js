6179211aae41c3c3dfb66886051eb40b82e5ab3b
Aziz Kale <59444395+kale1st@users.noreply.github.com>
2026-09-23 21:18:12 +0200

    Add Firebase configuration to ayarlar.js
    Added Firebase configuration settings for the application.

---
+++ ayarlar.js
@@ -0,0 +1,21 @@
+// Firebase konsolundaki "firebaseConfig" nesnesini aşağıya yapıştırın.
+// Boş bırakılırsa uygulama önizleme modunda çalışır (veriler sadece bu tarayıcıda).
+window.FIREBASE_AYAR = {
+  apiKey: "AIzaSyCBDhhGBks4LsYvCTXBeMWgFfGCKrTP2DA",
+  authDomain: "canli-quiz.firebaseapp.com",
+  projectId: "canli-quiz",
+  storageBucket: "canli-quiz.firebasestorage.app",
+  messagingSenderId: "725164128469",
+  appId: "1:725164128469:web:7dd8b6f7203d6aa8110a5c"
+};
+
+/* Örnek:
+window.FIREBASE_AYAR = {
+  apiKey: "AIza...",
+  authDomain: "canli-quiz-xxxx.firebaseapp.com",
+  projectId: "canli-quiz-xxxx",
+  storageBucket: "canli-quiz-xxxx.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdef"
+};
+*/
